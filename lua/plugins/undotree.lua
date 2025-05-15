68f5b315a3e3a413984b2cdbe40eb31904710ce0
JoelMTom <joelqwerty007@gmail.com>
2025-05-15 12:51:29 +0530

    remove transparency in undo tree win

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,9 @@
 return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
-  config = true,
+  opts = {
+      window = { winblend = 0,},
+  },
   keys = { -- load the plugin only when using it's keybinding:
     { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
   },
