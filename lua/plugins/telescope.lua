2c9b862407684e20b2fd93fdc431eab6be83989d
JoelMTom <joelqwerty007@gmail.com>
2025-05-25 15:16:59 +0530

    change build to make in telescope-fzf-native and add cmd to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,62 +1,64 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  -- tag = "0.1.5",
-  dependencies = { "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-fzf-native.nvim", },
-  opts = {
-    extensions = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = "smart_case",
-      }
-    }
-  },
-  keys = {
-    {
-      "<leader>ff",
-      function()
-        require("telescope.builtin").find_files()
-      end,
-      desc = "Telescope find files",
+    "nvim-telescope/telescope.nvim",
+    -- tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
     },
-    {
-      "<leader>fg",
-      function()
-        require("telescope.builtin").live_grep()
-      end,
-      desc = "Telescope live grep",
+    cmd = { "Telescope", },
+    opts = {
+        extensions = {
+            fzf = {
+                fuzzy = true,
+                override_generic_sorter = true,
+                override_file_sorter = true,
+                case_mode = "smart_case",
+            }
+        }
     },
-    {
-      "<leader>fb",
-      function()
-        require("telescope.builtin").buffers()
-      end,
-      desc = "Telescope buffers",
+    keys = {
+        {
+            "<leader>ff",
+            function()
+                require("telescope.builtin").find_files()
+            end,
+            desc = "Telescope find files",
+        },
+        {
+            "<leader>fg",
+            function()
+                require("telescope.builtin").live_grep()
+            end,
+            desc = "Telescope live grep",
+        },
+        {
+            "<leader>fb",
+            function()
+                require("telescope.builtin").buffers()
+            end,
+            desc = "Telescope buffers",
+        },
+        {
+            "<leader>fh",
+            function()
+                require("telescope.builtin").help_tags()
+            end,
+            desc = "Telescope help tags",
+        },
+        {
+            "<C-p>",
+            function()
+                require("telescope.builtin").git_files()
+            end,
+            desc = "Telescope git files",
+        },
+        {
+            "<leader>fd",
+            function()
+                require("telescope.builtin").fd()
+            end
+        }
     },
-    {
-      "<leader>fh",
-      function()
-        require("telescope.builtin").help_tags()
-      end,
-      desc = "Telescope help tags",
-    },
-    {
-      "<C-p>",
-      function()
-        require("telescope.builtin").git_files()
-      end,
-      desc = "Telescope git files",
-    },
-    -- {
-    --   "<leader>fd",
-    --   function ()
-    --     require("telescope.builtin").fd()
-    --   end
-    -- }
-  },
-  config = function()
-    require("telescope").load_extension("fzf")
-  end
+    config = function()
+        require("telescope").load_extension("fzf")
+    end
 }
