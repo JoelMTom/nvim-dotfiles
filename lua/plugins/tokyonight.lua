4f954f7f5987a245df86faf3c35deaa0fe3860c9
JoelMTom <joelqwerty007@gmail.com>
2026-06-22 16:26:28 +0530

    Add background transparency in tokyonight theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,9 +1,19 @@
+local opts = {
+  transparent = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  }
+}
+
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd([[colorscheme tokyonight-night]])
+  opts = function(_, _)
+    vim.schedule(function()
+      vim.cmd([[ colorscheme tokyonight-night ]])
+    end)
+    return opts
   end,
 }
